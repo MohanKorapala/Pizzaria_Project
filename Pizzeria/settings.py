7cfe46a267d96c859014a15c1f2038f599bb0f2e
Mohan Korapala <Mohan_korapala@baylor.edu>
2022-05-06 19:15:04 -0500

    5/6/22

--- Pizzeria/settings.py
+++ Pizzeria/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statiic/'
 
 MEDIA_URL = 'MainApp/static/mainapp/media'
 MEDIA_URL = os.path.join(BASE_DIR,'media')
